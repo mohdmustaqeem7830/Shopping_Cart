c29d282af7031e85d42faf9a55f5bb90da04a97a
mohdmustaqeem7830 <mustakimraza38@gmail.com>
2025-04-17 14:08:43 +0530

    front end complete for the login register and home

---
+++ src/main/java/com/ecom/Shopping_Cart/Controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.ecom.Shopping_Cart.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+     @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
